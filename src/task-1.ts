b48c308eb577e9ad29677b9e56de42e10cdcc172
Alla-Nazarenko <umnyj.start@gmail.com>
2025-11-25 16:11:38 +0100

    task 6 corrected

--- src/task-1.ts
+++ src/task-1.ts
@@ -6,3 +6,13 @@ console.log(`Name: ${name}`);
 console.log(`Age: ${age}`);
 console.log(`Online: ${isOnline}`);
 console.log("");
+
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  const random: number = Math.random();
+
+  if (random < 0.5) {
+    setTimeout(reject, 1000, new Error("Error!"));
+  } else {
+    setTimeout(resolve, 1000, random);
+  }
+});
